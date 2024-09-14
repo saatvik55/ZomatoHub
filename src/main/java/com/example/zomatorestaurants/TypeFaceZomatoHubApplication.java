19c2b8d2898178b6e34334d5d89ba16d118bbeab
saatvik55 <saatviksethi55@gmail.com>
2024-09-14 21:53:49 +0530

    Initial setup

---
+++ src/main/java/com/example/zomatorestaurants/TypeFaceZomatoHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.zomatorestaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeFaceZomatoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeFaceZomatoHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zomatorestaurants/TypeFaceZomatoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zomatorestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeFaceZomatoHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
